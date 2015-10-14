27c03a9fc837dc610bba1f329149b643f9d0a392
Edouard DUPIN <yui.heero@gmail.com>
2015-10-14 21:21:03 +0200

    [DEV] update new lutin 0.8.0

--- lutin_ogg.py
+++ lutin_ogg.py
@@ -2,13 +2,29 @@
 import lutin.module as module
 import lutin.tools as tools
 import lutin.debug as debug
+import os
+
+
+def get_type():
+	return "LIBRARY"
 
 def get_desc():
 	return "Ogg-tremor library for ogg audio decoding"
 
+def get_licence():
+	return "BSD-3"
 
-def create(target):
-	my_module = module.Module(__file__, 'ogg', 'LIBRARY')
+def get_compagny_type():
+	return "org"
+
+def get_compagny_name():
+	return "xiph"
+
+def get_version():
+	return [1,3,0]
+
+def create(target, module_name):
+	my_module = module.Module(__file__, module_name, get_type())
 	my_module.add_src_file([
 		'ogg/ogg/framing.c',
 		'ogg/ogg/bitwise.c',
@@ -26,15 +42,39 @@ def create(target):
 		'ogg/tremor/info.c',
 		'ogg/tremor/mapping0.c'
 		])
-	my_module.compile_version_CC(1989, gnu=True)
+	my_module.compile_version("c", 1989, gnu=True)
 	my_module.compile_flags('c', "-Wno-duplicate-decl-specifier")
 	if target.name=="Android":
 		my_module.compile_flags('c', "-DBYTE_ORDER=1")
 		my_module.compile_flags('c', "-DBIG_ENDIAN=0")
 		my_module.compile_flags('c', "-DLITTLE_ENDIAN=1")
-	my_module.add_export_path(tools.get_current_path(__file__) + "/ogg")
-	my_module.add_path(tools.get_current_path(__file__)+"/ogg/ogg/")
-	my_module.add_path(tools.get_current_path(__file__)+"/ogg/tremor/")
+	my_module.add_path(os.path.join(tools.get_current_path(__file__), "ogg"))
+	my_module.add_path(os.path.join(tools.get_current_path(__file__), "ogg/ogg"))
+	my_module.add_path(os.path.join(tools.get_current_path(__file__), "ogg/tremor"))
+	my_module.add_header_file([
+		'ogg/tremor/window_lookup.h',
+		'ogg/tremor/mdct.h',
+		'ogg/tremor/misc.h',
+		'ogg/tremor/mdct_lookup.h',
+		'ogg/tremor/registry.h',
+		'ogg/tremor/codebook.h',
+		'ogg/tremor/config_types.h',
+		'ogg/tremor/lsp_lookup.h',
+		'ogg/tremor/block.h',
+		'ogg/tremor/asm_arm.h',
+		'ogg/tremor/codec_internal.h',
+		'ogg/tremor/ivorbiscodec.h',
+		'ogg/tremor/os.h',
+		'ogg/tremor/window.h',
+		'ogg/tremor/backends.h',
+		'ogg/tremor/ivorbisfile.h'
+		],
+		destination_path="tremor")
+	my_module.add_header_file([
+		'ogg/ogg/ogg.h',
+		'ogg/ogg/os_types.h'
+		],
+		destination_path="ogg")
 	return my_module
 
 
