ba85eb4d8c2184e57803adb4c14193447efedbcb
Edouard DUPIN <yui.heero@gmail.com>
2016-10-04 23:41:29 +0200

    [DEV] Update new lutin 2.2.0 (no legacy support)

--- lutin_ogg.py
+++ lutin_ogg.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python
-import lutin.module as module
-import lutin.tools as tools
 import lutin.debug as debug
+import lutin.tools as tools
 import os
 
 
@@ -23,63 +22,64 @@ def get_compagny_name():
 def get_version():
 	return [1,3,0]
 
-def create(target, module_name):
-	my_module = module.Module(__file__, module_name, get_type())
+def configure(target, my_module):
 	my_module.add_src_file([
-		'ogg/ogg/framing.c',
-		'ogg/ogg/bitwise.c',
-		'ogg/tremor/floor0.c',
-		'ogg/tremor/vorbisfile.c',
-		'ogg/tremor/synthesis.c',
-		'ogg/tremor/res012.c',
-		'ogg/tremor/block.c',
-		'ogg/tremor/mdct.c',
-		'ogg/tremor/sharedbook.c',
-		'ogg/tremor/codebook.c',
-		'ogg/tremor/floor1.c',
-		'ogg/tremor/window.c',
-		'ogg/tremor/registry.c',
-		'ogg/tremor/info.c',
-		'ogg/tremor/mapping0.c'
-		])
+	    'ogg/ogg/framing.c',
+	    'ogg/ogg/bitwise.c',
+	    'ogg/tremor/floor0.c',
+	    'ogg/tremor/vorbisfile.c',
+	    'ogg/tremor/synthesis.c',
+	    'ogg/tremor/res012.c',
+	    'ogg/tremor/block.c',
+	    'ogg/tremor/mdct.c',
+	    'ogg/tremor/sharedbook.c',
+	    'ogg/tremor/codebook.c',
+	    'ogg/tremor/floor1.c',
+	    'ogg/tremor/window.c',
+	    'ogg/tremor/registry.c',
+	    'ogg/tremor/info.c',
+	    'ogg/tremor/mapping0.c'
+	    ])
 	my_module.compile_version("c", 1989, gnu=True)
 	my_module.add_flag('c', "-Wno-duplicate-decl-specifier")
 	if "Android" in target.get_type():
 		my_module.add_flag('c', "-DBYTE_ORDER=1")
 		my_module.add_flag('c', "-DBIG_ENDIAN=0")
 		my_module.add_flag('c', "-DLITTLE_ENDIAN=1")
-	my_module.add_path(os.path.join(tools.get_current_path(__file__), "ogg"))
-	my_module.add_path(os.path.join(tools.get_current_path(__file__), "ogg/ogg"))
-	my_module.add_path(os.path.join(tools.get_current_path(__file__), "ogg/tremor"))
+	my_module.add_path([
+	    "ogg",
+	    "ogg/ogg",
+	    "ogg/tremor"
+	    ])
 	my_module.add_header_file([
-		'ogg/tremor/window_lookup.h',
-		'ogg/tremor/mdct.h',
-		'ogg/tremor/misc.h',
-		'ogg/tremor/mdct_lookup.h',
-		'ogg/tremor/registry.h',
-		'ogg/tremor/codebook.h',
-		'ogg/tremor/config_types.h',
-		'ogg/tremor/lsp_lookup.h',
-		'ogg/tremor/block.h',
-		'ogg/tremor/asm_arm.h',
-		'ogg/tremor/codec_internal.h',
-		'ogg/tremor/ivorbiscodec.h',
-		'ogg/tremor/os.h',
-		'ogg/tremor/window.h',
-		'ogg/tremor/backends.h',
-		'ogg/tremor/ivorbisfile.h'
-		],
-		destination_path="tremor")
+	    'ogg/tremor/window_lookup.h',
+	    'ogg/tremor/mdct.h',
+	    'ogg/tremor/misc.h',
+	    'ogg/tremor/mdct_lookup.h',
+	    'ogg/tremor/registry.h',
+	    'ogg/tremor/codebook.h',
+	    'ogg/tremor/config_types.h',
+	    'ogg/tremor/lsp_lookup.h',
+	    'ogg/tremor/block.h',
+	    'ogg/tremor/asm_arm.h',
+	    'ogg/tremor/codec_internal.h',
+	    'ogg/tremor/ivorbiscodec.h',
+	    'ogg/tremor/os.h',
+	    'ogg/tremor/window.h',
+	    'ogg/tremor/backends.h',
+	    'ogg/tremor/ivorbisfile.h'
+	    ],
+	    destination_path="tremor")
 	my_module.add_header_file([
-		'ogg/ogg/ogg.h',
-		'ogg/ogg/os_types.h'
-		],
-		destination_path="ogg")
+	    'ogg/ogg/ogg.h',
+	    'ogg/ogg/os_types.h'
+	    ],
+	    destination_path="ogg")
 	my_module.add_depend([
-		    'c',
-		    'm'
-		    ])
-	return my_module
+	    'c',
+	    'm'
+	    ])
+	return True
 
 
 
